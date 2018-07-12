c1be204fcc2bb01a1783bc78ed26ea3ef2f316a4
Raykgeneer96 <EvilGeniusesAppsCompany@gmail.com>
2018-07-12 01:40:52 -0500

    Se añaden todos los fragment para agregar documentos

--- app/src/main/java/com/evilgeniuses/sistemicpublications/MainActivity.java
+++ app/src/main/java/com/evilgeniuses/sistemicpublications/MainActivity.java
@@ -1,7 +1,10 @@
 package com.evilgeniuses.sistemicpublications;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.Login:
+                Intent intent   =   new Intent(MainActivity.this, IniciarSesionActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
